042e3f295a3b218dab2562a4250c6437fe80b5c8
jpencola <paulpencola@gmail.com>
2011-05-23 22:26:04 -0400

    Passing relevant Puzzle data back to the template.

--- bumblebee/puzzle/views.py
+++ bumblebee/puzzle/views.py
@@ -4,8 +4,15 @@ from django.template import RequestContext
 from bumblebee.puzzle.models import Puzzle
 
 def show_puzzle(request, guid="DEFAULT"):
-    puzzle = Puzzle.objects.get(guid__exact=guid)
-    response_data = {puzzle: puzzle}
+    puzzle = Puzzle.objects.select_related().get(guid = guid)
+    response_data = {"puzzle":{
+                         "name": puzzle.name,
+                         "width": puzzle.width,
+                         "height": puzzle.height,
+                         "max_rows": puzzle.difficulty.grid.rows,
+                         "max_cols": puzzle.difficulty.grid.columns
+                         }
+                     }
     return render_to_response("puzzle.html",
                               response_data,
                               mimetype="text/html",
