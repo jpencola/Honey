513edf185b7909047b2f39e2e6f3445434121059
jpencola <paulpencola@gmail.com>
2011-05-23 22:43:22 -0400

    More template fun! The Puzzle's image URL is now dynamic.

--- bumblebee/puzzle/views.py
+++ bumblebee/puzzle/views.py
@@ -5,12 +5,14 @@ from bumblebee.puzzle.models import Puzzle
 
 def show_puzzle(request, guid="DEFAULT"):
     puzzle = Puzzle.objects.select_related().get(guid = guid)
+    first_associated_image = puzzle.imagedetail_set.all()[0]
     response_data = {"puzzle":{
                          "name": puzzle.name,
                          "width": puzzle.width,
                          "height": puzzle.height,
                          "max_rows": puzzle.difficulty.grid.rows,
-                         "max_cols": puzzle.difficulty.grid.columns
+                         "max_cols": puzzle.difficulty.grid.columns,
+                         "image_url": first_associated_image.aviary_url,
                          }
                      }
     return render_to_response("puzzle.html",
