70195045d7121f61a85e85cc98a49bb5cbc55305
jpencola <paulpencola@gmail.com>
2011-10-01 09:31:44 -0400

    Adds 404 and 500 templates and handlers.

--- bumblebee/puzzle/urls.py
+++ bumblebee/puzzle/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from bumblebee.puzzle import views
 
+handler404 = 'views.http_fourhundredfour_error'
+handler500 = 'views.http_fivehundred_error'
+
 urlpatterns = patterns('',
     url(r'(?P<guid>[a-zA-Z0-9_=.-]+)/$', views.show_puzzle, name='puzzle'),
     url(r'(?P<guid>[a-zA-Z0-9_=.-]+)$', views.show_puzzle, name='puzzle'),
--- bumblebee/puzzle/views.py
+++ bumblebee/puzzle/views.py
@@ -3,6 +3,18 @@ from django.template import RequestContext
 
 from bumblebee.puzzle.models import Puzzle
 
+def http_fourhundredfour_error(request):
+    return render_to_response("404.html",
+                              {},
+                              mimetype="text/html",
+                              context_instance=RequestContext(request))
+    
+def http_fivehundred_error(request):
+    return render_to_response("500.html",
+                              {},
+                              mimetype="text/html",
+                              context_instance=RequestContext(request))
+
 def show_puzzle(request, guid="DEFAULT"):
     puzzle = Puzzle.objects.select_related().get(guid = guid)
     first_associated_image = puzzle.imagedetail_set.all()[0]
